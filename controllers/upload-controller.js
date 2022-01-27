482c5da534ec8f096783e052c85c2bf6024659d0
Ulukbek <ulukbekkaparbekov@gmail.com>
2022-01-27 19:28:56 +0600

    Add ts

--- controllers/upload-controller.js
+++ controllers/upload-controller.js
@@ -1,5 +1,6 @@
 const fs = require('fs')
 const util = require('util')
+const {nanoid} = require("nanoid")
 const {pipeline} = require('stream')
 const pump = util.promisify(pipeline)
 
@@ -7,7 +8,12 @@ async function routes(fastify, options) {
     fastify.post('/upload-template', async (req, reply) => {
         try {
             const data = await req.file()
-            await pump(data.file, fs.createWriteStream(data.filename))
+            console.log(Object.keys(data))
+            // console.log("fields: ", Object.keys(req.))
+            // Object.keys(req).forEach((key) => {
+            //     console.log(key + " ", req[key], "\n")
+            // })
+            await pump(data.file, fs.createWriteStream("./uluk/" + nanoid()))
             reply.send()
         } catch (e) {
             console.log("error: ", e)
